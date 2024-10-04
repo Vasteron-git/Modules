afa7c858ab1cb61b3b4d6956c2c80e823ce6394b
Vasteron-Git <Larmax@yandex.ru>
2024-10-04 22:23:16 +0300

    house

---
+++ module_5_4.py
@@ -0,0 +1,61 @@
+
+class House:
+    houses_history = []
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __new__(cls,  *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            for i in range(1, new_floor+1):
+                print(f"Этаж: { i } {self.name}")
+
+    def __eq__(self, other):
+        if isinstance(self, House) and isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        else:
+            print("Значения не принадлежат классу House")
+    def __lt__(self, other):   #( <)
+        return self.number_of_floors < other.number_of_floors
+    def __le__(self, other):    #( <=):
+        return self.number_of_floors <= other.number_of_floors
+    def __gt__(self, other): #( >)
+        return self.number_of_floors > other.number_of_floors
+    def __ge__(self, other):  #( >=)
+        return self.number_of_floors >= other.number_of_floors
+    def __ne__(self, other):  #( !=)
+        return self.number_of_floors != other.number_of_floors
+    def __add__(self, value):
+        if isinstance(self.number_of_floors, int):
+            self.number_of_floors = self.number_of_floors + value
+    def __radd__(self, value):
+         return House.__add__(self, value)
+    def __iadd__(self, value):
+        return House.__add__(self, value)
+    def __len__(self):
+        return self.number_of_floors
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
+
